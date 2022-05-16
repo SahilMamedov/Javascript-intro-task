b0e7fc260fff3cb117199db71d5d55cd298f41a8
SahilMamedov <sahilim@code.edu.az>
2022-05-17 01:15:10 +0400

    Feat Homework

---
+++ javascript.js
@@ -0,0 +1,70 @@
+
+//(1) Bir method olsun iki parametr qəbul etsin
+//  və bu parametrlərdən birincini ikinciyə bölüb nəticəni geri qaytarsin.
+
+// function sum (bolunen,bolen){
+
+// return bolunen/bolen
+
+// }
+// console.log(sum(40,4));
+
+//(2) Repeat deyə bir method olsun və iki parametr qəbul etsin
+//  biri "word" digəri "count" yəni bu şəkildə "Repeat(word, count)"
+//   bu method göndərilən word-ü göndərilən count qədər yan-yana yazdırıb geri qaytarsın.
+
+
+// function repatet(word,count){
+//     let = result="";
+// for(let i=0; i<count;i++){
+    
+//     result+=word;
+// }
+// }
+// repatet("ha",6);
+// console.log(result)
+
+
+//(3) Bir method olsun göndərilən "n" sayda ədədin hamsını toplayıb geri qaytarsın.
+
+
+// function cem(){
+//     let num=0;
+// for(let i=0;i<arguments.length;i++){
+
+// num+=arguments[i];
+
+// }
+// return num
+
+// }
+// console.log(cem(4,20,76));
+
+
+//(4) Sahe deyə Method(lar) yaradılır.
+
+// Çevrənin sahəsi
+
+// function sahe(radius){
+
+//   return (radius*radius)*3
+
+// }
+// console.log(sahe(2))
+
+// Düzbucaqlının sahəsi
+
+// function sahe(en,uzunluq){
+//     return en*uzunluq
+// }
+// console.log(sahe(3,5))
+
+// Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi
+
+// function sahe(radius,a,b,c){
+   
+//     let p=(a+b+c)/2;
+//     let s = radius*p;
+//     return s
+// }
+// console.log(sahe(8,5,7,9))
